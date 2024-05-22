0f21ee1bd617e3227a75c8cbf911664fffaa7d06
Drew Graham <drewgraham.business@gmail.com>
2024-05-22 14:31:35 -0600

    Bring experiment_recorder local

--- openstl/simulation/__init__.py
+++ openstl/simulation/__init__.py
@@ -1,6 +1,6 @@
 from .simulation_dataset import SimulationDataset
 from .array_type import ArrayType
-import experiment_recorder
+from . import experiment_recorder
 
 __all__ = [
     'SimulationDataset', 'ArrayType', 'experiment_recorder'
