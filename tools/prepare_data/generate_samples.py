4348fda80a3c967ba0e79e0c94304d0bd3b0c7b6
Drew Graham <drewgraham.business@gmail.com>
2024-05-23 14:22:15 -0600

    data changes

--- tools/prepare_data/generate_samples.py
+++ tools/prepare_data/generate_samples.py
@@ -4,10 +4,7 @@ import os
 from argparse import ArgumentParser as ArgParser
 from openstl.simulation.experiment_recorder import generate_experiment_record, save_experiment_record
 
-from openstl.simulation.simulations import simulations
 from openstl.simulation.generation import create_samples
-from openstl.simulation.utils import get_simulation_class
-from openstl.simulation.preparation import normalize_samples
 
 
 # Create a parser with additional arguments specific to dataset generation.
@@ -15,9 +12,6 @@ def create_local_parser():
     parser = ArgParser()
 
     parser.description = "Generate a dataset for a simulation."
-
-    parser.add_argument('--simulation', type=str, choices=[simulation.__name__ for simulation in simulations],
-                        help='Determines the simulation type.', required=True)
 
     parser.add_argument('--increment', type=int, default=5,
                         help="Only applies to Boiling simulation, sets the increment value.")
@@ -35,26 +29,13 @@ def create_local_parser():
 def main():
     args = create_local_parser().parse_args()  # Parse the command line arguments.
 
-    # Get the simulation class.
-    simulation_class = get_simulation_class(args.simulation)
-    if not simulation_class:
-        raise ValueError(f"Invalid simulation: {args.simulation}")
-
-    simulation = simulation_class(args)  # Initialize the simulation class.
-
-    if not os.path.exists(args.datafolder):
-        raise FileNotFoundError(f"Data folder in path {args.datafolder} does not exist.")
-
     start_time = time.time()  # Record the start time.
     # Generate samples based on the provided parameters.
-    create_samples(args.total_sample_length, simulation, args.datafolder, verbose=True)
-    if args.normalize:
-        normalize_samples(args.datafolder, simulation.vmin, simulation.vmax)  # Normalize the samples.
+    create_samples(args.total_sample_length, args.datafolder, args.normalize, args, verbose=True)
 
     elapsed = time.time() - start_time  # Calculate the elapsed time.
 
     samples_arguments = {
-        'simulation': args.simulation,
         'increment': args.increment,
         'total_sample_length': args.total_sample_length,
         'normalize': args.normalize
