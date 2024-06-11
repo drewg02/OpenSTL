6f016e3d5fc71ce3661a27baab956772005fccaa
Drew Graham <drewgraham.business@gmail.com>
2024-06-11 08:14:56 -0600

    for dist

---
+++ tools/dist_train_sim.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+PYTHON=${PYTHON:-"python"}
+
+GPUS=$1
+PY_ARGS=${@:2}
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+WORK_DIR=$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/
+
+$PYTHON -m torch.distributed.launch \
+    --nnodes=$NNODES \
+    --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR \
+    --nproc_per_node=$GPUS \
+    --master_port=$PORT \
+    tools/train_sim.py --dist \
+    --ex_name $WORK_DIR \
+    --launcher="pytorch" ${PY_ARGS}
