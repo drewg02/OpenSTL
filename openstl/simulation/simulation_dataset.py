685f9f48ba5a186db3bdf1b605325d79647b4b98
Drew Graham <drewgraham.business@gmail.com>
2024-07-09 15:33:35 -0600

    Add support for .npz and .pkl files

--- openstl/simulation/simulation_dataset.py
+++ openstl/simulation/simulation_dataset.py
@@ -1,7 +1,25 @@
 import numpy as np
 import torch
 from torch.utils.data import Dataset
+import pickle
 
+def load_data(sample):
+    data = []
+    for file in sample:
+        if isinstance(file, str):
+            if file.endswith('.npy'):
+                loaded_file = np.load(file)
+            elif file.endswith('.npz'):
+                loaded_file = np.load(file)['data']
+            elif file.endswith('.pkl'):
+                with open(file, 'rb') as f:
+                    loaded_file = pickle.load(f)
+            else:
+                raise ValueError("Unsupported file format")
+        else:
+            loaded_file = np.array(file)
+        data.append(loaded_file)
+    return np.stack(data)
 
 class SimulationDataset(Dataset):
     def __init__(self, data, pre_seq_length, aft_seq_length):
@@ -17,8 +35,7 @@ class SimulationDataset(Dataset):
     def __getitem__(self, idx):
         sample = self.data['samples'][idx]
 
-        data = [np.load(file) if isinstance(file, str) else np.array(file) for file in sample]
-        data = np.stack(data)
+        data = load_data(sample)
 
         x_sequence = data[:self.pre_seq_length]
         y_sequence = data[self.pre_seq_length:self.pre_seq_length + self.aft_seq_length]
