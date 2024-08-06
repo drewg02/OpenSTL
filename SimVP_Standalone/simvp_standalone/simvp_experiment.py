af06cef17c921d23363fa8176a7db73b808b3dcd
Drew Graham <drewgraham.business@gmail.com>
2024-08-06 09:51:07 -0600

    fix local_rank

--- SimVP_Standalone/simvp_standalone/simvp_experiment.py
+++ SimVP_Standalone/simvp_standalone/simvp_experiment.py
@@ -147,7 +147,7 @@ class SimVP_Experiment():
                 print('Using native PyTorch AMP. Training in mixed precision (fp16).')
         else:
             print('AMP not enabled. Training in float32.')
-        self.model = NativeDDP(self.model, device_ids=[self.args.local_rank],
+        self.model = NativeDDP(self.model, device_ids=[self.local_rank],
                                broadcast_buffers=self.args.broadcast_buffers,
                                find_unused_parameters=self.args.find_unused_parameters)
 
